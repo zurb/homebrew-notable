20ec01e1abb26dcb561d15f46ad24532684ee4ab
Jordan Humphreys <jordan@mailyard.net>
2016-05-27 22:18:48 -0700

    Update formula to conform more with homebrew standards.

--- notable.rb
+++ notable.rb
@@ -1,13 +1,11 @@
 class Notable < Formula
-  desc "Command line interface for ZURB Notable"
+  desc "A command-line interface for ZURB Notable, a design feedback service."
   homepage "http://zurb.com/notable"
   url "https://github.com/zurb/notable-cli/archive/0.0.5.tar.gz"
-  version "0.0.5"
   sha256 "a30d582d76504569900e7194b55cc1c9f56f03d43ee5638aa4c453b5ecc21316"
   depends_on "wget"
 
   def install
     bin.install "notable"
   end
-
 end
