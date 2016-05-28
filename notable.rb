1f79cb83414e16c1782eedb3fd2c7070a5139548
Jordan Humphreys <jordan@mailyard.net>
2016-05-27 22:20:06 -0700

    Remove indefinite article.

--- notable.rb
+++ notable.rb
@@ -1,5 +1,5 @@
 class Notable < Formula
-  desc "A command-line interface for ZURB Notable, a design feedback service."
+  desc "Notable is a command-line interface for ZURB Notable, a design feedback service."
   homepage "http://zurb.com/notable"
   url "https://github.com/zurb/notable-cli/archive/0.0.5.tar.gz"
   sha256 "a30d582d76504569900e7194b55cc1c9f56f03d43ee5638aa4c453b5ecc21316"
