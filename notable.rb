2412f77ff767ed547df94b01f928890516b3f9a3
Jordan Humphreys <jordan@mailyard.net>
2016-05-27 22:21:23 -0700

    Remove name from description.

--- notable.rb
+++ notable.rb
@@ -1,5 +1,5 @@
 class Notable < Formula
-  desc "Notable is a command-line interface for ZURB Notable, a design feedback service."
+  desc "command-line interface for zurb.com/notable, a design feedback service."
   homepage "http://zurb.com/notable"
   url "https://github.com/zurb/notable-cli/archive/0.0.5.tar.gz"
   sha256 "a30d582d76504569900e7194b55cc1c9f56f03d43ee5638aa4c453b5ecc21316"
