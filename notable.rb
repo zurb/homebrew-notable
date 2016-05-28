710989d12e523b668988f3ae71f34ad3e11ad71c
Jordan Humphreys <jordan@mailyard.net>
2016-05-27 19:03:36 -0700

    Update formla.

--- notable.rb
+++ notable.rb
@@ -4,6 +4,7 @@ class Notable < Formula
   url "https://github.com/zurb/notable-cli/archive/0.0.2.tar.gz"
   version "0.0.2"
   sha256 "f5c9982b745d48450ff08d0515066d1deaa8a34122af60c93a283e17b69dc315"
+  depends_on "wget"
 
   def install
     bin.install "notable"
