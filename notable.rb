f297fc1458befef7444475df404b9ace3a73557c
Jordan Humphreys <jordan@mailyard.net>
2016-05-28 09:46:00 -0700

    Bump version.

--- notable.rb
+++ notable.rb
@@ -1,8 +1,8 @@
 class Notable < Formula
   desc "command-line interface for design feedback by ZURB"
   homepage "http://zurb.com/notable"
-  url "https://github.com/zurb/notable-cli/archive/0.0.6.tar.gz"
-  sha256 "fc2ae505edf48e6d1e9010875a264c5119357b7a3d4977926e04fb318d7c18d8"
+  url "https://github.com/zurb/notable-cli/archive/0.0.8.tar.gz"
+  sha256 "46e0129262b8bb9b60abc8ceb50a3a26e74b88e24d3a6d9e185e1234cfc66b3e"
   depends_on "wget"
 
   def install
